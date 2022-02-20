12db539293225eb06b943b8063951f777ded1d8c
Radheesh R D <99800128+Rdhrd6776@users.noreply.github.com>
2022-02-20 22:39:56 +0530

    Day -1 Assignment

---
+++ Day - 1 Assignment (HTML & CSS).py
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello World!</title>
+  </head>
+  <body>
+      <h1 style="color : red" class="title">Hello World! </h1>
+      <p style="color: blue"> I am very new to web dev and still at the early stages of learning. <br>I just wanted to say that the way you write really helps me understand these concepts much more than other tutorials.<hr> Thank you so much!!!Reply </p>
+      <span style="color: green">"hi all , how are you</span>
+      <img src=https://bitarray.io/images.logo.png alt="bitarray logo">
+      <a href="https://www.ggogle.com">google</a>
+      <button> click me</button>
+      <u>this is nothing</u>
+      <strong>"welcome you all"</Strong>
+  </body>
+</html>
